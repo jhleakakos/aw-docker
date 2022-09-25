932c689f6c0d91011f60e55e875c349424ef5196
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2022-09-25 11:30:47 -0600

    add sql script to restore adventureworks db

---
+++ restore_script.sql
@@ -0,0 +1,7 @@
+USE [master]
+RESTORE DATABASE [AdventureWorks2019] 
+FROM  DISK = N'/var/opt/mssql/AdventureWorks2019.bak' WITH FILE = 1
+,  MOVE N'AdventureWorks2017' TO N'/var/opt/mssql/data/AdventureWorks2019.mdf'
+,  MOVE N'AdventureWorks2017_log' TO N'/var/opt/mssql/data/AdventureWorks2019_log.ldf'
+,  NOUNLOAD
+,  STATS = 5
